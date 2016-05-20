81c9574b3b6e2af7b5514793aad07bf3ea9551d2
stefan.marx <smarx@marxworld.net>
2016-05-20 14:51:27 +0200

    Added Groovy stand allone example using grapes

---
+++ src/main/samples/sample.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+
+@Grab(group='com.appdynamics.ace.cli', module='CommandFramework', version='1.5')
+import com.appdynamics.ace.util.cli.api.api.CommandlineExecution
+import com.appdynamics.ace.util.cli.api.groovy.CommandWrapper
+
+println("READY")   ;
+CommandlineExecution cle = new CommandlineExecution("GroovyCli");
+cle.setHelpVerboseEnabled(false);
+
+cle.addCommand(new CommandWrapper("test","TEST",
+        [ string: [desc:"string to print",args:true,opt:false]], {p ->
+
+    println "Hello $p.string"
+}));
+
+cle.execute(args);
